0d6fcad843217fef55d6b78714e6ecbcb859236a
Aleksey Belchenkov <u608110@gmail.com>
2018-08-04 19:03:30 +0300

    Adding a Context Reducer For Actions

--- contactmanager/src/components/Contact.js
+++ contactmanager/src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Consumer } from "../context";
 
 class Contact extends Component {
 
@@ -13,38 +14,47 @@ class Contact extends Component {
         });
     };
 
-    onDeleteClick = () => {
-        this.props.deleteClickHandler();
+    onDeleteClick = (id, dispatch) => {
+        dispatch({
+            type: 'DELETE_CONTACT',
+            payload: id
+        });
     };
 
     render() {
-        const { name, email, phone } = this.props.contact;
+        const { id, name, email, phone } = this.props.contact;
         const { showContactInfo } = this.state;
         return (
-            <div id="content" className="card card-body mb-3">
-                <h5>
-                    {name}{' '}
-                    <i
-                        onClick={this.onShowClick}
-                        className="fas fa-sort-down cur-p"
-                    />
-                    <i
-                        className="fas fa-times"
-                        onClick={this.onDeleteClick}
-                    />
-                </h5>
-                { showContactInfo ? (<ul className="list-group">
-                    <li className="list-group-item">Email: {email}</li>
-                    <li className="list-group-item">Phone: {phone}</li>
-                </ul>) : null }
-            </div>
+            <Consumer>
+                {value => {
+                    const { dispatch } = value;
+                    return (
+                        <div id="content" className="card card-body mb-3">
+                            <h5>
+                                {name}{' '}
+                                <i
+                                    onClick={this.onShowClick}
+                                    className="fas fa-sort-down cur-p"
+                                />
+                                <i
+                                    className="fas fa-times"
+                                    onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                                />
+                            </h5>
+                            { showContactInfo ? (<ul className="list-group">
+                                <li className="list-group-item">Email: {email}</li>
+                                <li className="list-group-item">Phone: {phone}</li>
+                            </ul>) : null }
+                        </div>
+                    )
+                }}
+            </Consumer>
         );
     }
 }
 
 Contact.propTypes = {
-    contact: PropTypes.object.isRequired,
-    deleteClickHandler: PropTypes.func.isRequired
+    contact: PropTypes.object.isRequired
 };
 
 export default Contact;
