588e971c37a96259ee62ceb578dbfcfb87f61faf
Aleksey Belchenkov <u608110@gmail.com>
2018-08-11 17:57:11 +0300

    Form Submit Action To Context

--- contactmanager/src/components/contacts/AddContact.js
+++ contactmanager/src/components/contacts/AddContact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { Consumer } from "../../context";
+import uuid from 'uuid';
 
 class AddContact extends Component {
     state = {
@@ -12,59 +13,86 @@ class AddContact extends Component {
         [e.target.name]: e.target.value
     });
 
-    onSubmit = (e) => {
+    onSubmit = (dispatch, e) => {
       e.preventDefault();
-      console.log(this.state);
+      const { name, email, phone } = this.state;
+      const newContact = {
+          id: uuid(),
+          name,
+          email,
+          phone
+      };
+
+      dispatch({
+          type: 'ADD_CONTACT',
+          payload: newContact
+      });
+
+      // Clear State
+      this.setState({
+          name: '',
+          email: '',
+          phone: ''
+      });
     };
 
     render() {
         const { name, email, phone } = this.state;
+
         return (
-            <div className="my-4 card">
-                <h2 className="card-header">Добавить контакт</h2>
-                <div className="card-body">
-                    <form onSubmit={this.onSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="name">Имя</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Введите имя ..."
-                                name="name"
-                                value={name}
-                                onChange={this.onChange}
-                            />
+            <Consumer>
+                {value => {
+                    const { dispatch } = value;
+
+                    return (
+                        <div className="my-4 card">
+                            <h2 className="card-header">Добавить контакт</h2>
+                            <div className="card-body">
+                                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                                    <div className="form-group">
+                                        <label htmlFor="name">Имя</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="Введите имя ..."
+                                            name="name"
+                                            value={name}
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            className="form-control"
+                                            placeholder="Введите Email ..."
+                                            value={email}
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="phone">Телефон</label>
+                                        <input
+                                            type="phone"
+                                            name="phone"
+                                            className="form-control"
+                                            placeholder="Введите Телефон ..."
+                                            value={phone}
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
+                                    <input
+                                        type="submit"
+                                        value="Добавить контакт"
+                                        className="btn btn-block btn-primary"
+                                    />
+                                </form>
+                            </div>
                         </div>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="email"
-                                name="email"
-                                className="form-control"
-                                placeholder="Введите Email ..."
-                                value={email}
-                                onChange={this.onChange}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="phone">Телефон</label>
-                            <input
-                                type="phone"
-                                name="phone"
-                                className="form-control"
-                                placeholder="Введите Телефон ..."
-                                value={phone}
-                                onChange={this.onChange}
-                            />
-                        </div>
-                        <input
-                            type="submit"
-                            value="Добавить контакт"
-                            className="btn btn-block btn-primary"
-                        />
-                    </form>
-                </div>
-            </div>
+                    );
+                }}
+            </Consumer>
         );
     }
 }
