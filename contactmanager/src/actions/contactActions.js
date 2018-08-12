bedd29ccd963dfb789749a9d6cff13264b2f064c
Aleksey Belchenkov <u608110@gmail.com>
2018-08-12 18:45:12 +0300

    GET Request Dispatch

--- contactmanager/src/actions/contactActions.js
+++ contactmanager/src/actions/contactActions.js
@@ -3,11 +3,15 @@ import {
     DELETE_CONTACT,
     GET_CONTACTS
 } from "./types";
+import axios from 'axios';
 
-export const getContacts = () => {
-    return {
-        type: GET_CONTACTS
-    }
+export const getContacts = () => async dispatch => {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+    dispatch({
+        type: GET_CONTACTS,
+        payload: res.data
+    });
 };
 
 export const deleteContact = (id) => {
